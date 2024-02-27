3a85255e39aedea2acd598fd059ff1439051f025
reshetovProg <94145533+reshetovProg@users.noreply.github.com>
2024-02-27 21:24:27 +0300

    Add files via upload

---
+++ src/main/java/com/example/jwtsecur/JwtsecurApplication.java
@@ -0,0 +1,14 @@
+package com.example.jwtsecur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtsecurApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JwtsecurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jwtsecur/JwtsecurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtsecur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtsecurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
